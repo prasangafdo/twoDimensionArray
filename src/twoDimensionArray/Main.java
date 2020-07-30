650d64bcf9598849b43b3bda8ed60053782277cf
Prasanga Fernando <prasangafdz@outlook.com>
2020-07-30 14:54:58 +0530

    minor changes

---
+++ src/twoDimensionArray/Main.java
@@ -0,0 +1,36 @@
+package twoDimensionArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		for(int i = 0;i<=5;i++) {
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i =0;i<=5;i++) {
+			for(int j=i;j<=5;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<=i*2;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		for(int i=0;i<=5;i++) {
+			for(int j=i;j<=5;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
